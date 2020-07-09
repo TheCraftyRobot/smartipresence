bcb532912484559b84a498561e7a52e01b4a43af
TheCraftyRobot <47324936+TheCraftyRobot@users.noreply.github.com>
2020-07-09 16:11:26 +0100

    sub height 125%

--- app.js
+++ app.js
@@ -190,7 +190,7 @@ function initializeSession() {
 	  session.subscribe(event.stream, 'subscriber', {
 	    insertMode: 'append',
 	    width: '100%',
-	    height: '100%'
+	    height: '125%'
 	  }, handleError);
 	});
 
